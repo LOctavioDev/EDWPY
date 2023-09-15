8840c03fab47172cd768f97fe4d1011845654027
LOctavioDev <octaviodevtech@gmail.com>
2023-09-15 02:06:54 -0600

    Dict and methods

---
+++ DICCIONARIOS/dict3.py
@@ -0,0 +1,16 @@
+precios = {
+    'MANZANA' : 12.0,
+    'BANANA' : 12.32,
+    'KIWI' : 34.7
+    }
+
+claves = precios.keys()
+valores = precios.values()
+items = precios.items()
+
+precios['melon'] = 12.39
+
+#ITERACION DE UN DICCIONARIO CON FOR
+
+for fruta, precio in precios.items():
+    print(f'EL PRECIO DE {fruta} ES DE ${precio}')
